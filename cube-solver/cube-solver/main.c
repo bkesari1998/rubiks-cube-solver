dd481ed6578985d88f2d58a22b61ca24f9336be4
Bharat Kesari <bkesari1998@gmail.com>
2020-07-03 00:13:34 -0400

    Initial project commit

---
+++ cube-solver/cube-solver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cube-solver
+//
+//  Created by Bharat Kesari on 7/3/20.
+//  Copyright © 2020 Bharat Kesari. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
